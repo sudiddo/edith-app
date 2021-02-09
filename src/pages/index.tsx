0b0eed02eec026e54962471ffc76041f9c3838ed
sudiddo <su.diddo@gmail.com>
2021-02-09 22:08:41 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className='min-h-screen bg-white'>
+      <div className='flex flex-col justify-center items-center p-10 '>
+        <div className='font-cinzel text-3xl text-gold z-10'>EDITH</div>
+        <div className='font-cinzel text-xl text-blue-dark'>
+          "Even Dead, I'm The Hero"
+        </div>
+      </div>
+      <div>
+        <div className='mx-5 pb-5'>
+          <div className='font-cinzel text-md'>Hall of fame</div>
+          <hr className='bg-blue-dark' />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  important: true,
+  purge: false,
+  // purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+      cinzel: ['Cinzel', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#231F20',
+        },
+        orange: {
+          DEFAULT: '#ff800c',
+        },
+        white: {
+          DEFAULT: '#EAEAEA',
+        },
+        gold: {
+          dark: '#8E793E',
+          DEFAULT: '#AD974F',
+        },
+        blue: {
+          dark: '#0B354D',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
